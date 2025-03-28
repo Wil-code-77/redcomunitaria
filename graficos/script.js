5bbad9b7c1fb07dd1b56d929dda0da681f83938f
Diego-Cp17 <diego931130+1@gmail.com>
2025-03-27 23:20:41 -0500

    Se mejoran aspecto importantes del grafico 1

--- graficos/script.js
+++ graficos/script.js
@@ -1,33 +1,36 @@
-
-
-const xValues = ["Medellin", "Sabaneta", "Envigado", "Bello", "Caldas", "la Estrella"];
-const yValues = [55, 49, 44, 24, 15];
+const xValues = [
+  "Medellin",
+  "Sabaneta",
+  "Envigado",
+  "Bello",
+  "Caldas",
+  "la Estrella",
+];
+const yValues = [55, 49, 44, 24, 15, 5];
 const barColors = [
-  "#f3ad0b", 
+  "#f3ad0b",
   "#eec514",
   "#ebd644",
   "#faf203",
   "#e3e901",
-  "#c1e409"
+  "#c1e409",
 ];
 
 new Chart("myChart", {
   type: "bar",
   data: {
     labels: xValues,
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
+    datasets: [
+      {
+        backgroundColor: barColors,
+        data: yValues,
+      },
+    ],
   },
   options: {
-      title: {
+    title: {
       display: true,
-      text: "Territorios con mayor numero de mepresas lideradas por mujeres"
-    }
-  }
+      text: "Territorios con mayor numero de empresas lideradas por mujeres en el area metropolitana",
+    },
+  },
 });
-
-
-
-
